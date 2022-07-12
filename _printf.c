5d806523f431e31982c9b5c953c9c2a28e3412cc
Adeboye Victor <adeboyevictor18@gmail.com>
2022-07-11 17:38:26 -0700

    new update

--- _printf.c
+++ _printf.c
@@ -26,7 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/*write(1, &format[i], 1);*/
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
